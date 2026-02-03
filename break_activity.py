d976a54fde3bdc02a510118c767c958ef0777d96
tejaskopperla-code <tejas.kopperla@gmail.com>
2026-02-03 23:23:25 +0530

    Add files via upload

---
+++ break_activity.py
@@ -0,0 +1,7 @@
+a = input("Enter a word to chech if it has A(write all the words in capital only one A will count and automatically ends the program )  :    ")
+for i in a:
+    if (i =='A'):
+        print("A is found")
+        break
+    else:
+        print("A not foound in this position or word")
